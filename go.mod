1221ae808d594736a6f2c873b66c96c81cd0cc26
K3vinchaparro <collegek450@gmail.com>
2024-06-26 17:01:10 -0500

    Inicializacion de repositorio

---
+++ go.mod
@@ -0,0 +1,7 @@
+
+module sga_horario_mid
+
+go 1.21
+
+require github.com/astaxie/beego v1.12.1
+require github.com/smartystreets/goconvey v1.6.4
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "sga_horario_mid/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
